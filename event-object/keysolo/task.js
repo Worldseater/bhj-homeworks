12b6e5419037cb359ee37aaa4dc0f49fe84c7bc3
Worldseater <vsdll@mail.ru>
2025-06-05 18:05:29 +0300

    first commit keysolo

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -16,16 +16,19 @@ class Game {
     this.lossElement.textContent = 0;
   }
 
-  registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода символа вызываем this.success()
-      При неправильном вводе символа - this.fail();
-      DOM-элемент текущего символа находится в свойстве this.currentSymbol.
-     */
-  }
+registerEvents() {
+  document.addEventListener('keyup', (event) => {
+    const inputChar = event.key.toLowerCase();
+    const currentChar = this.currentSymbol.textContent.toLowerCase();
+
+    if (inputChar === currentChar) {
+      this.success();
+    } else {
+      this.fail();
+    }
+  });
+}
+
 
   success() {
     if(this.currentSymbol.classList.contains("symbol_current")) this.currentSymbol.classList.remove("symbol_current");
